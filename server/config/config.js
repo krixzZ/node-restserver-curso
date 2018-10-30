60e4ee92e5aa9ae3df20ff4ecbb6f3a44df5e062
Cristian <crixtop5@hotmail.com>
2018-10-30 17:44:18 -0300

    Primer commit

---
+++ server/config/config.js
@@ -0,0 +1,7 @@
+// process es un objeto que depende del
+// entorno o enviroment end onde corre
+
+// =========================
+// Puerto
+// =========================
+process.env.PORT = process.env.PORT || 3000;
